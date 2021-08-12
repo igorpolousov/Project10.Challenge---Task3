77d7bbef492c9a7a18b8ebe43b5a5a4af408a592
Igor Polousov <igorpolousov@gmail.com>
2021-08-12 17:11:57 +0300

    Initial Commit

---
+++ Project10.Challenge - Task3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10.Challenge - Task3
+//
+//  Created by Igor Polousov on 12.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
